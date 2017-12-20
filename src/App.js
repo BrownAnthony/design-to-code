93f8c7cc41b40fd729c9e016bd3f1e7924584c7f
Anthony Brown <chaoticferocity@gmail.com>
2017-12-20 17:40:25 -0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './App.css';
+
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container Header" >
+        <div className="row">
+          <div className="col-md">
+            {/* <img src="" alt/> */}
+            <h1>Default</h1>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+
+export default App;
